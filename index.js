965c85fba8a508019223ca064fac625f5c74cd7d
lalyre <c_lalyre@yahoo.fr>
2023-04-29 21:21:59 +0200

    cl

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict';
 
-function monthly_payment(capital, credit_repayment_rate, years_count) {
-	const a = capital * credit_repayment_rate / 12;
+function yearly_payment(capital, credit_repayment_rate, years_count) {
+	const a = capital * credit_repayment_rate;
 	const b = (1 + credit_repayment_rate/12)**(-12*years_count);
 	const c = 1 - b;
 	const d = a/c;
 	return d;
-}
+};
 
-module.exports = {
+
+function monthly_payment(capital, credit_repayment_rate, years_count) {
+	const a = yearly_payment(capital, credit_repayment_rate, years_count) / 12;
+	return a;
+};
+
+
+/*
+module.exports  {
+	yearly_payment,
 	monthly_payment,
-}
+};
+*/
